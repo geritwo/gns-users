9619076aceca9675fe5c39333a3d51fddd150ed0
Gergo Vamosi <geritwo@gmail.com>
2022-10-23 22:43:42 +0200

    Add authentication, create boilerplate DB model

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
+import os
 from flask import Flask, request, jsonify
+from flask_httpauth import HTTPBasicAuth
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
+# Init app
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
 
-def validate_login(username, password):
-    pass
+# Extensions
+db = SQLAlchemy(app)
+db.init_app(app)
+auth = HTTPBasicAuth
+
+
+class User(db.Model):
+    __tablename__ = 'users'  # Override default
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), unique=True, nullable=False, index=True)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+    last_login = db.Column(db.DateTime)
+
+    def hash_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 @app.route("/", methods=['GET'])
@@ -12,29 +35,25 @@ def hello():
     return "<p>Hello World!</p>"
 
 
-@app.route("/users/list", methods=['GET'])
+@app.route("/api/users", methods=['GET', 'POST'])
+# List users or add user
 def list_users():
     pass
 
 
-@app.route("/users/create", methods=['POST'])
-def add_user():
-    pass
-
-
-@app.route("/users/update", methods=['PUT'])
+@app.route("api/users/<int:id>", methods=['PUT', 'DELETE'])
+# Update or delete user
 def update_user():
     pass
 
 
-@app.route("/users/delete", methods=['POST'])
-def delete_user():
-    pass
+@app.route("api/login")
+@auth.login_required
+def get_profile():
+    return f"Hello, {auth.current_user()}! Login successful"
 
 
-@app.route("/login/<username>:<password>", methods=['POST'])
-def login(username, password):
-    return {
-        'user': username,
-        'pass': password
-    }
+if __name__ == '__main__':
+    if not os.path.exists('db.sqlite'):
+        db.create_all()
+    app.run(debug=True)
